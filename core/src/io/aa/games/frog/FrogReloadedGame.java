db971985f01ae8c272f7a3c08c87254d8b39b58e
Enrico Rubboli <rubboli@gmail.com>
2014-04-27 13:25:12 +0200

    print sprite coord

--- core/src/io/aa/games/frog/FrogReloadedGame.java
+++ core/src/io/aa/games/frog/FrogReloadedGame.java
@@ -5,19 +5,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 
 public class FrogReloadedGame extends ApplicationAdapter {
     FrogSprite frog;
+    SpriteBatch batch;
     OrthographicCamera camera;
+    BitmapFont font;
+
     private static int SCREEN_WIDTH=800;
     private static int SCREEN_HEIGHT=480;
 
     @Override
     public void create () {
         camera = new OrthographicCamera();
-        frog = new FrogSprite(10,240, camera);
+        batch = new SpriteBatch();
+        frog = new FrogSprite(10,240, camera, batch);
         camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
+        font = new BitmapFont();
     }
 
     @Override
@@ -26,7 +33,12 @@ public class FrogReloadedGame extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         detectInput();
         camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
         frog.draw();
+        font.draw(batch, frog.toString(), 20, 20);
+        batch.end();
+
     }
 
     public void detectInput(){
@@ -40,10 +52,10 @@ public class FrogReloadedGame extends ApplicationAdapter {
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
-            if(touchPos.x > frog.x){ frog.moveLeft(); }
-            if(touchPos.x < frog.x){ frog.moveRight(); }
-            if(touchPos.y > frog.y){ frog.moveDown(); }
-            if(touchPos.y < frog.y){ frog.moveUp(); }
+            if(touchPos.x > frog.x){ frog.moveRight(); }
+            if(touchPos.x < frog.x){ frog.moveLeft(); }
+            if(touchPos.y > frog.y){ frog.moveUp(); }
+            if(touchPos.y < frog.y){ frog.moveDown(); }
 
         }
     }
--- core/src/io/aa/games/frog/FrogSprite.java
+++ core/src/io/aa/games/frog/FrogSprite.java
@@ -2,53 +2,56 @@ package io.aa.games.frog;
 
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
  * Created by erubboli on 4/27/14.
  */
 public class FrogSprite {
-    private SpriteBatch batch;
+    private Batch batch;
     private Texture texture;
     public int x = 0;
     public int y = 0;
     private Camera cam;
     private int steps = 24;
 
-    public FrogSprite(int x, int y, Camera cam){
+    public FrogSprite(int x, int y, Camera cam, Batch batch){
       texture = new Texture("frog.png");
-      batch = new SpriteBatch();
+      this.batch = batch;
       this.x = x; this.y = y;
       this.cam = cam;
     }
 
 
     public void moveRight(){
-        if( x - steps >= 0 ){
-            x -= steps;
-        }
-    }
-    public void moveLeft(){
         if( x + steps < cam.viewportWidth) {
             x += steps;
         }
     }
 
-    public void moveDown(){
-        if( y + steps < cam.viewportHeight){
-            y += steps;
+    public void moveLeft(){
+        if( x - steps >= 0 ){
+            x -= steps;
         }
     }
-    public void moveUp() {
+
+    public void moveDown(){
         if (y - steps >= 0) {
             y -= steps;
         }
     }
+    public void moveUp() {
+        if( y + steps < cam.viewportHeight){
+            y += steps;
+        }
+    }
 
     public void draw(){
-        batch.setProjectionMatrix(cam.combined);
-        batch.begin();
-        batch.draw(texture, x,  y);
-        batch.end();
+        batch.draw(texture, x, y);
+    }
+
+    public String toString(){
+       return "Frog is at "+ Integer.toString(x) + ":" + Integer.toString(y);
     }
 }
